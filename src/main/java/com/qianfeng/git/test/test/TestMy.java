85522e28b9f8de64e30d51e78b5f74e8b77d475c
fanhuny <another@git.com>
2018-11-18 19:59:42 +0800

    Idea的第一次

---
+++ src/main/java/com/qianfeng/git/test/test/TestMy.java
@@ -0,0 +1,19 @@
+package com.qianfeng.git.test.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author FangYu
+ * @Date 2018/11/18
+ */
+@Controller
+@RequestMapping("git")
+public class TestMy {
+    @RequestMapping("hello")
+    public String hello(){
+        System.out.println("fanhuny");
+        return "hello";
+    }
+}
